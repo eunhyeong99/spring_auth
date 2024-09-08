757ee349b8f66dfe01110a5e3d24602f153adfa0
whdms <whdmsgud8@naver.com>
2024-09-08 15:40:09 +0900

    Filter

--- src/main/java/com/sparta/spring_auth/controller/UserController.java
+++ src/main/java/com/sparta/spring_auth/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.sparta.spring_auth.controller;
 
+import com.sparta.spring_auth.dto.SignupRequestDto;
+import com.sparta.spring_auth.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/user/login-page")
     public String loginPage() {
@@ -16,5 +24,11 @@ public class UserController {
     @GetMapping("/user/signup")
     public String signupPage() {
         return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(@RequestBody SignupRequestDto requestDto) {
+        userService.signup(requestDto);
+        return "login";
     }
 }
